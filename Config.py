5cf958c228a8c035885f99d21633fa92a62b8439
kinvekt <antonabramov2000@gmail.com>
2018-01-04 22:55:04 +0700

    Добавил файл с описанием проекта

--- Config.py
+++ Config.py
@@ -3,16 +3,14 @@ SecretKey = "***REMOVED***"
 
 QuotedCurrency = 'USD'
 Quantity = 5
-CurrencyMinQuantity = 0.001
+#CurrencyMinQuantity = 0.001
 Profit = 0.01
 StockFee = 0.01
-
 Period = 60 
 
+#Private setings
 Balance = []
 MainBalance = 0
-
 MaxPrice = 0
-
 TradedCurrency = []
 Orders = []
